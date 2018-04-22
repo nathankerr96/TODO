00c580f8f4ba7ffb083de0f19c54f4cdc32f106f
unknown <nathankerr96@gmail.com>
2018-04-21 19:15:15 -0700

    push db_acess

---
+++ db_access.js
@@ -0,0 +1,39 @@
+const mysql = require('mysql');
+
+
+exports.handle_request = function (req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'})
+
+
+  var config = {
+    host: "localhost",
+    user: "test",
+    password: "test1234",
+    database: "todo",
+    //insecureAuth: true
+  }
+
+  config.authSwitchHandler = (unused, cb) => {
+    // workaround for node mysql bug #1507
+    if (pluginName === 'auth_socket') {
+      cb(null, Buffer.alloc(0));
+    } else {
+      cb(new Error("Unsupported auth plugin"));
+    }
+  };
+
+  console.log("HERE")
+
+  var con = mysql.createConnection(config);
+
+  con.connect(function(err) {
+    if (err) {
+      throw err;
+    } else {
+      alert("Connected!");
+    }
+  })
+
+  //res.write(JSON.stringify({a: 1, b: 2}));
+  res.end();
+}
