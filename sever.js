1c044f94f0b7df69f185a4ebbf374922dee99407
unknown <nathankerr96@gmail.com>
2018-04-21 17:48:29 -0700

    Initial commit

---
+++ sever.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const db = require('./db_access.js')
+
+
+function route(req, res) {
+
+    var parsed_url = url.parse(req.url, true);
+    var filename = "." + parsed_url.pathname;
+
+    if (filename === './api') {
+      db.handle_request(req, res);
+    } else {
+      fs.readFile(filename, function(err, data) {
+          if (err) {
+              res.writeHead(404, {'Content-Type': 'text/html'});
+              console.log(filename)
+              return res.end("404 Not Found");
+          }
+
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+          return res.end();
+      });
+    }
+}
+
+http.createServer(route).listen(8080);
